b4a99a7dbc5abaa727cd437f63a87ff063837e56
Adam Hoffman <wintros@yahoo.com>
2020-03-13 22:28:17 -0600

    fffffffffff

--- client/src/components/ToDoList.js
+++ client/src/components/ToDoList.js
@@ -1,15 +1,24 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState } from 'react'
 
 const ToDoList = () => {
-const [task, setTask] = useState([])
+const [task, setTask] = useState({TaskName: ""})
+  
 
-useEffect(() => {
-    setTask(task)
-}, [task])
+const ChangeHandler = e => {    
+    setTask( {
+        [e.target.name]: e.target.value
+    })
+}
 
-const Submit = e => {
-    e.preventDefault()
-    setTask(task)
+
+const Submit = newTask => {
+    newTask.preventDefault()   
+    const NewestTask = {
+        TaskName: newTask
+    }
+    setTask({
+        task: [...task, NewestTask]
+    })
 }
 console.log("task", task)
 
@@ -17,11 +26,11 @@ console.log("task", task)
         <div>
             <form className="form">
                 <input 
-                type="text"
-                placeholder="newtask"
+                type="text"               
                 className="input"
-                value={task}
-                onChange={({ target: { value }}) => setTask(value)}                
+                name="TaskName"                
+                value={task.TaskName}    
+                onChange={ChangeHandler}                            
                 />
                 <button onClick={Submit}>Enter New Task</button>
             </form>
